d4bfe84319e4aff2494db1244a2b7d2b00a6da2c
Joe Wandy <joe.wandy@glasgow.ac.uk>
2018-06-30 20:41:51 +0100

    Use BasePoolMiner.Mode.SMART constant for the mode

--- miner/src/SushiPoolMiner.js
+++ miner/src/SushiPoolMiner.js
@@ -14,9 +14,9 @@ class SushiPoolMiner extends BasePoolMiner {
      * @param {number} deviceId
      * @param {string} deviceName
      */
-    constructor(mode, blockchain, accounts, mempool, time, address, deviceId, deviceName, startDifficulty) {
+    constructor(blockchain, accounts, mempool, time, address, deviceId, deviceName, startDifficulty) {
         let extraData = new Uint8Array(0);
-        super(mode, blockchain, accounts, mempool, time, address, deviceId, extraData);
+        super(BasePoolMiner.Mode.SMART, blockchain, accounts, mempool, time, address, deviceId, extraData);
         this._deviceName = deviceName;
         this.on('share', (block, fullValid) => this._onBlockMined(block, fullValid));
         this._startDifficulty = startDifficulty;
@@ -43,7 +43,7 @@ class SushiPoolMiner extends BasePoolMiner {
         Nimiq.Log.i(SushiPoolMiner, `Registering to pool using device id ${this._deviceId} (${deviceName}) as a smart client.`);
         this._send({
             message: 'register',
-            mode: 'smart',
+            mode: this.mode,
             address: this._ourAddress.toUserFriendlyAddress(),
             deviceId: this._deviceId,
             startDifficulty: this._startDifficulty,
